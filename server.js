c23d5d9085ad558686b9877e0f8b1658bb8fac34
Neeraj Tomar <41513668+neerajtomar98@users.noreply.github.com>
2022-04-22 15:00:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const jsonServer = require("json-server");
-const app = jsonServer.create();
-const path = require("path");
-const express = require("express");
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create();
 const router = jsonServer.router("data.json");
-const port = process.env.PORT || 3001;
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
 
-app.use("/data", middlewares, router);
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+server.use(middlewares);
+server.use(router);
 
 server.listen(port);
